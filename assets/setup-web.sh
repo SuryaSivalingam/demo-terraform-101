cb2a33dbc08704e2299b10532dd1bca1f703afdf
SuryaSivalingam <148701777+SuryaSivalingam@users.noreply.github.com>
2025-04-20 13:46:04 +0530

    Update setup-web.sh

--- assets/setup-web.sh
+++ assets/setup-web.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-cp /tmp/assets/webapp /usr/local/bin/
+cp /tmp/ws/assets/webapp /usr/local/bin/
 chmod +x /usr/local/bin/*
-cp /tmp/assets/webapp.service /lib/systemd/system/webapp.service
+cp /tmp/ws/assets/webapp.service /lib/systemd/system/webapp.service
 service webapp start
 systemctl enable webapp
 
